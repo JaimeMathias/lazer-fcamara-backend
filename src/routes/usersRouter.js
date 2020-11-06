e97dfb8ddfea189ba94138b47baaef9235fdcd5b
jgsouzadev <souza.joaoguilherme2002@gmail.com>
2020-11-06 15:34:42 -0300

    fix-add bodyparser

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 const Services = require('../services/users')
 const Delete = require('../services/delete')
+const Update = require('../services/update')
 const usersRouter = Router();
 
 usersRouter.get('/', Services.UsersGetAll);
-usersRouter.get('/delete/:id', Delete.DeleteById);
+usersRouter.delete('/delete/:id', Delete.DeleteById);
+usersRouter.put('/update/:id', Update.UpdateById);
 
 
 export default usersRouter;
