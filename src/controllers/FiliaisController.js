830ae36239d3bb249694f9c43d2cd17990204416
João Guilherme <57119331+jgsouzadev@users.noreply.github.com>
2020-11-15 22:12:34 -0300

    fix(*): code struture

--- src/controllers/FiliaisController.js
+++ src/controllers/FiliaisController.js
@@ -1,31 +1,17 @@
 const {Filials} = require ('../models')
 
-
 module.exports = new class createFilials {
 
 async store (request, response) {
-
   try {
+    const { filial1 } = request.body;
+    const FilialCreate = await Filials.create({location : filial1})
 
-    const { filial1, filial2 } = request.body;
-
-    const filialSP = await Filials.create ({
-
-      location : filial1
-    })
-
-    const filialSTS = await Filials.create ({
-
-      location : filial2
-    })
-
-      response.status(200).json({"msg": "Criado com sucesso!"})
+    response.status(200).json({"msg": "Criado com sucesso!"})
 
     } catch (error) {
-
       console.log (error)
       response.status(400).json({"msg": "Falha no cadastro"})
-
     }
   }
 }
