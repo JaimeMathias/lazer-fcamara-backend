c537586bce34ec1b0552d86fe1274aec73c3b4d6
João Guilherme <57119331+jgsouzadev@users.noreply.github.com>
2020-11-18 12:11:51 -0300

    feat(*): platforms

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,6 @@ import Router from 'express';
 import usersRouter from "./usersRouter";
 import authsRouter from "./authRoutes";
 import filialsRouter from './filialsRoutes';
-import rowsRouter from './rowsRoutes';
 import platformsRouter from './platformsRoutes';
 
 const bodyParser = require('body-parser');
