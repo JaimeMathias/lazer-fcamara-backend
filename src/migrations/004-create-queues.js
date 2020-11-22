8c44de8de3d5cb612df6ceba2e1a451bb6e89cb7
jgsouzadev <souza.joaoguilherme2002@gmail.com>
2020-11-22 20:03:04 -0300

    fix(*): migration bug

--- src/migrations/004-create-queues.js
+++ src/migrations/004-create-queues.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Rows', {
+    await queryInterface.createTable('Queues', {
       id:{
         allowNull: false,
         autoIncrement: true,
@@ -40,6 +40,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Rows');
+    await queryInterface.dropTable('Queues');
   }
 };
