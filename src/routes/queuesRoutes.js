c7c1b8b6627d24d684c604b440ec89819c38eeb5
jgsouzadev <souza.joaoguilherme2002@gmail.com>
2020-11-22 20:04:03 -0300

    feat(*): add join and quit routes

--- src/routes/queuesRoutes.js
+++ src/routes/queuesRoutes.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 const QueueController = require('../controllers/QueueController');
+const AuthController = require('../controllers/AuthController')
 
 const queuesRoutes = Router();
 
 // Criar as rotas abaixo
+
+queuesRoutes.put('/', AuthController.Auth ,QueueController.store)
+queuesRoutes.put('/sair', AuthController.Auth ,QueueController.quit)
 
 export default queuesRoutes;
