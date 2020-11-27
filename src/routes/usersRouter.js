04fa0cb0acf19fca6f2588e070af9092bbaad541
Joao Guilherme <souza.joaoguilherme2002@gmail.com>
2020-11-27 17:51:37 -0300

    fix(*): routes

--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -5,9 +5,10 @@ const AuthController = require('../controllers/AuthController')
 const usersRouter = Router();
 
 usersRouter.post('/', UsersController.store);
-usersRouter.get('/', AuthController.Auth ,UsersController.indexAll);
+usersRouter.get('/' ,UsersController.indexAll);
 usersRouter.get('/:id', AuthController.Auth , UsersController.index);
 usersRouter.put('/:id', AuthController.Auth, UsersController.update);
 usersRouter.delete('/:id', AuthController.Auth, UsersController.remove);
+
 
 export default usersRouter;
