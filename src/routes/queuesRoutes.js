cbd9ccdf6d013bdf38656fd8d5df7116f731f623
jgsouzadev <souza.joaoguilherme2002@gmail.com>
2020-11-28 20:49:21 +0000

    feat(server): integrated to frontend

--- src/routes/queuesRoutes.js
+++ src/routes/queuesRoutes.js
@@ -7,8 +7,8 @@ const queuesRoutes = Router();
 // Criar as rotas abaixo
 
 queuesRoutes.get('/' ,QueueController.queueData)
-queuesRoutes.get('/polling', AuthController.Auth, QueueController.polling)
+queuesRoutes.put('/polling', AuthController.Auth, QueueController.polling)
 queuesRoutes.put('/', AuthController.Auth ,QueueController.store)
-queuesRoutes.put('/sair', AuthController.Auth ,QueueController.quit)
+queuesRoutes.put('/exit', AuthController.Auth ,QueueController.quit)
 
 export default queuesRoutes;
